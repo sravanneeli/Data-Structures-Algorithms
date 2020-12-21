3f10cacd01b63eb140aa2122b107d3f04a540cda
sravanneeli <sravan.neeli267@gmail.com>
2020-12-21 19:12:08 +0530

    commit

---
+++ Arrays/min_steps_infinite_grid.py
@@ -0,0 +1,12 @@
+def coverPoints(A, B):
+    steps = 0
+    for i in range(1,len(A)):
+        steps += max(abs(A[i]-A[i-1]),abs(B[i]-B[i-1]))
+    return steps
+
+def main():
+    A = [0, 1, 4]
+    B = [0, 1, 4]
+    print("Minimum steps to cover the start point to end point:",coverPoints(A, B))
+
+main()
