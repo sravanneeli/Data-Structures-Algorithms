2d0301dec4835011d969d418b27bdde6c2fdad4f
sravanneeli <sravan.neeli267@gmail.com>
2020-12-21 17:21:08 +0530

    commit

---
+++ miscellaneous/count_trail_zeros_factorial.py
@@ -0,0 +1,13 @@
+def count_trail_zeros(n):
+    i = 5
+    count = 0
+    while (n/i >= 1):
+        count += int(n/i)
+        i *= 5
+    return count
+
+def main():
+    n = int(input("Enter a number: "))
+    print("Number of trailing zeros of given number factorial: ", count_trail_zeros(n))
+
+main()
