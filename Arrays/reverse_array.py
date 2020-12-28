b76679a041a7178765937dd0a0764ae636ee3e57
sravanneeli <sravan.neeli267@gmail.com>
2020-12-28 23:28:08 +0530

    commit

--- Arrays/reverse_array.py
+++ Arrays/reverse_array.py
@@ -7,8 +7,17 @@ def reverse(arr):
         j -= 1
     return arr
 
+def checksorted(arr):
+    for i in range(len(arr)-1):
+        if arr[i] > arr[i+1]:
+            return False
+    return True
+
+
 def main():
     arr = [4, 12, 9, 15, 6, 10, 7, 2, 3]
-    print(reverse(arr))
+    arrsorted = [4, 8, 13, 25, 23,  28, 33]
+    print("Reversed array",reverse(arr))
+    print("array is sorted or not ?", checksorted(arrsorted))
 
 main()
