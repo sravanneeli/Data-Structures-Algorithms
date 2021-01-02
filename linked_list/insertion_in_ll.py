041da64541be3b8bbe6942e5254fc9d403bc084e
sravanneeli <sravan.neeli267@gmail.com>
2021-01-02 23:31:20 +0530

    commit

--- linked_list/insertion_in_ll.py
+++ linked_list/insertion_in_ll.py
@@ -1,13 +1,14 @@
-from display_linked_list import create_ll
-
+from linked_list.display_linked_list import create_ll
 
 
 def insert(pos, x):
     pass
 
+
 def main():
-    A = [4 ,5, 12, 2, 1, 23, 5, 23]
+    A = [4, 5, 12, 2, 1, 23, 5, 23]
     ll = create_ll(A)
-    
+
+
 if __name__ == "__main__":
     main()
