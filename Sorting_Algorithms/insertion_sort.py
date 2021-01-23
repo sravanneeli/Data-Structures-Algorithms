69f85253158ed94eae6f3b0aa234e9730392543b
sravanneeli <sravan.neeli267@gmail.com>
2021-01-23 11:37:05 +0530

    commit

--- Sorting_Algorithms/insertion_sort.py
+++ Sorting_Algorithms/insertion_sort.py
@@ -1,24 +1,27 @@
-'''
+"""
     Insertion Sort:
     Worst Time Complexity   : O(n^2)
     Best Time Complexity    : O(n)
     Average Time Complexity : O(n^2)
-'''
+"""
+
+
 def insertion_sort(arr):
     n = len(arr)
     for i in range(1, n):
-        key = arr[i]
+        x = arr[i]
         j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j+1] = arr[j]
+        while j > -1 and arr[j] > x:
+            arr[j + 1] = arr[j]
             j -= 1
-        arr[j+1] = key
-
+        arr[j + 1] = x
     return arr
+
 
 def main():
     arr = [25, 17, 31, 13, 2]
     print("Sorted Array:", insertion_sort(arr))
-    
+
+
 if __name__ == "__main__":
     main()
