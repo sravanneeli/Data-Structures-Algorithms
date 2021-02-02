639d25cd811e8101fa48e5aaf90534e9b7e09f1c
sravanneeli <sravan.neeli267@gmail.com>
2021-02-02 09:23:02 +0530

    commit

--- Graphs/prims_algorithm.py
+++ Graphs/prims_algorithm.py
@@ -16,32 +16,33 @@ def prims(cost, near):
                 v = j
     t[0][0] = u
     t[1][0] = v
-    print(min_edge, u, v)
-    near[u] = near[v] = 0
-
+    near[u] = near[v] = -1
     for i in range(n):
-        if near[i] != 0:
+        if near[i] != -1:
             if cost[i][u] < cost[i][v]:
                 near[i] = u
             else:
                 near[i] = v
-    for i in range(1, n - 1):
+    print(u, v, min_edge)
+    for i in range(1, n-1):
         min_value = math.inf
         for j in range(n):
-            if cost[j][near[j]] < min_value and near[j] != 0:
+            if cost[j][near[j]] < min_value and near[j] != -1:
                 k = j
                 min_value = cost[j][near[j]]
 
+        print(k, near[k], min_value)
+
         t[0][i] = k
         t[1][i] = near[k]
-        near[k] = 0
-
+        near[k] = -1
+        # print(near)
         for j in range(n):
-            if near[j] != 0 and cost[j][k] < cost[j][near[j]]:
+            if near[j] != -1 and cost[j][k] < cost[j][near[j]]:
                 near[j] = k
 
-    for i in range(n - 1):
-        print(t[0][i], t[1][i])
+    # for i in range(n - 1):
+    #     print(t[0][i], t[1][i])
 
 
 def main():
@@ -51,7 +52,8 @@ def main():
             [6, 8, math.inf, math.inf, 9],
             [math.inf, 5, 7, 9, math.inf]]
 
-    near = [math.inf] * len(cost)
+    near = [math.inf for _ in range(len(cost))]
+    # print(near)
     prims(cost, near)
 
 
