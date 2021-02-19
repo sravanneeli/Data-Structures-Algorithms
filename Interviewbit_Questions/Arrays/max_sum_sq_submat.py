4790d5c5a42a00b3f8ce6b54dac1fe20a4489ec4
sravanneeli <sravan.neeli267@gmail.com>
2021-02-19 23:45:32 +0530

    commit

--- Interviewbit_Questions/Arrays/max_sum_sq_submat.py
+++ Interviewbit_Questions/Arrays/max_sum_sq_submat.py
@@ -1,51 +1,52 @@
+def valid(i, j, n):
+    return 0 <= i < n and 0 <= j < n
+
+
 def max_submat(A, B):
     n = len(A)
     print(n)
-    new = []
-    for i in range(n - B + 1):
-        new_temp = []
-        for col in range(n):
-            temp = 0
-            for j in range(i, i + B):
-                temp += A[j][col]
-            new_temp.append(temp)
-        new.append(new_temp)
-    print(new)
-    sub_Max_arr = []
-    for i in range(n - B + 1):
-        final = []
-        for col in range(n - B + 1):
-            temp = 0
-            for j in range(col, col + B):
-                temp += new[i][j]
-            final.append(temp)
-        sub_Max_arr.append(final)
-    print(len(sub_Max_arr))
-    print(len(sub_Max_arr[0]))
-    return max(max(sub_Max_arr))
+    ans = -10000000
+    for i in range(n):
+        for j in range(n):
+            if valid(i - 1, j, n):
+                A[i][j] += A[i - 1][j]
+            if valid(i, j - 1, n):
+                A[i][j] += A[i][j - 1]
+            if valid(i - 1, j - 1, n):
+                A[i][j] -= A[i - 1][j - 1]
+
+    print(A)
+    for i in range(n):
+        for j in range(n):
+            if i == B - 1 and j == B - 1:
+                print(1)
+                ans = max(ans, A[i][j])
+            if i == B - 1 and j - B >= 0:
+                print(2)
+                print(i, j-B)
+                curarea = A[i][j] - A[i][j - B]
+                ans = max(ans, curarea)
+            if j == B - 1 and i - B >= 0:
+                print(3)
+                print(j, i-B)
+                curarea = A[i][j] - A[i - B][j]
+                ans = max(ans, curarea)
+            if valid(i - B, j - B, n):
+                print(4)
+                print(i-B, j-B)
+                curarea = A[i][j] - A[i - B][j] - A[i][j - B] + A[i - B][j - B]
+                ans = max(ans, curarea)
+    return ans
 
 
 def main():
     A = [
-        [-33, 34, 0, 69, 24, -22, 58, 62, -36, 5, 45, -19, -73, 61, -9, 95],
-        [42, -73, -64, 91, -96, 2, 53, -8, 82, -79, 16, 18, -5, -53, 26, 71],
-        [38, -31, 12, -33, -1, -65, -6, 3, -89, 22, 33, -27, -36, 41, 11, -47],
-        [-32, 47, -56, -38, 57, -63, -41, 23, 41, 29, 78, 16, -65, 90, -58, -12],
-        [6, -60, 42, -36, -52, -54, -95, -10, 29, 70, 50, -94, 1, 93, 48, -71],
-        [-77, -16, 54, 56, -60, 66, 76, 31, 8, 44, -61, -74, 23, 37, 38, 18],
-        [-18, 29, 41, -67, 15, -61, -42, 4, 30, 77, 6, -27, 86, -79, 45, 24],
-        [-28, -30, -71, 77, 73, -3, 12, 86, -10, 61, -64, 55, 67, -45, 74, -69],
-        [-48, 50, 50, 41, 24, 66, -70, 7, 91, -93, 37, -43, -13, 53, 83, 45],
-        [9, -91, 58, -79, 88, -78, 46, 6, -70, -87, 68, 0, 91, 62, -45, -90],
-        [59, -76, 37, 48, -17, 95, -59, -98, 50, -9, -64, 74, -80, 96, -79, 48],
-        [99, -32, -16, -19, 34, -47, 99, -82, 38, 0, 88, 27, -33, 28, -7, -52],
-        [-17, -93, -79, 10, -83, -87, 14, 9, -84, 35, -49, -100, -51, 19, 56, 98],
-        [3, -76, -92, -56, -91, 89, 2, 95, -15, -7, 43, 23, 87, 14, 3, -52],
-        [-100, -42, -82, 80, 96, 98, -19, 89, 98, -91, 57, -28, -78, 38, -8, -62],
-        [79, 90, -43, 58, 91, -85, -12, 56, 11, -98, -66, -28, -45, 28, -54, 62]
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
     ]
 
-    print(max_submat(A, 7))
+    print(max_submat(A, 2))
 
 
 if __name__ == '__main__':
